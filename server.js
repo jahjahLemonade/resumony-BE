3e645e7f85bc58008f7160944cecfbfd5e185696
Joshua L <joshua.lemay@ibm.com>
2024-10-21 11:28:47 +0300

    Fix cors

--- server.js
+++ server.js
@@ -17,10 +17,14 @@ app.use(express.json());
 app.use(express.static('public'));
 
 // cors middleware
-const corsOptions = {
-  origin: 'https://resumony-fe.vercel.app'
-};
-app.use(cors(corsOptions))
+const setCorsHeaders = (req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', 'https://resumony-fe.vercel.app');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+}
+
+app.use(cors(setCorsHeaders));
 
 const { initializeApp } = require("firebase/app")
 // const { getAnalytics } = require("firebase/analytics")
