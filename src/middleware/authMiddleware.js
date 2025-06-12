7d152c1e285104a24fb9280bef36447f15f0eabc
Shubham Khunt <55044317+shubhamkhunt04@users.noreply.github.com>
2025-06-12 20:50:39 +0530

    Console added

--- src/middleware/authMiddleware.js
+++ src/middleware/authMiddleware.js
@@ -3,12 +3,15 @@ import jwt from 'jsonwebtoken'
 
 export const authMiddleware = (req, res, next) => {
   const token = req.header('Authorization')?.split('Bearer ')?.[1]
+  console.log({token})
   if (!token) return next(createHttpError(401, {message: 'Access denied'}))
   try {
     const decoded = jwt.decode(token)
+    console.log({decoded})
     req.user = decoded
     next()
   } catch (error) {
+    console.log({error})
     return next(createHttpError(401, {message: 'Access denied'}))
   }
 }
