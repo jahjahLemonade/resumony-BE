68de0b5ae5da8ce6bf9468a772e45cb2ba32da00
jahjahLemonade <jspades93@gmail.com>
2025-07-15 11:52:40 +0300

    Debugging

--- src/controller/stripe.js
+++ src/controller/stripe.js
@@ -286,6 +286,10 @@ export const testStripeWebHook = async (req, res) => {
   const sig = req.headers["stripe-signature"];
   let event;
 
+  console.log("Received webhook event:", req.body, req.headers,req.params);
+  console.log("Stripe Signature:", sig);
+  console.log("Stripe Secret Key:", process.env.STRIPE_WEBHOOK_SECRET_KEY);
+
   try {
     event = stripe.webhooks.constructEvent(req.body, sig, process.env.STRIPE_WEBHOOK_SECRET_KEY);
 
