551260991aa99332cb6008f70068cc4c9949aa04
jahjahLemonade <jspades93@gmail.com>
2025-07-16 11:48:25 +0300

    Debugging Webhook

--- src/controller/stripe.js
+++ src/controller/stripe.js
@@ -287,6 +287,7 @@ export const testStripeWebHook = async (req, res) => {
   let event;
 
   try {
+    console.log("Received webhook event")
     event = stripe.webhooks.constructEvent(req.body.toString(), sig, process.env.STRIPE_WEBHOOK_SECRET_KEY);
     console.log(">")
     // Handle different event types
