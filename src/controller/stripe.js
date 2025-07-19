cb7cd815e87f8c2e04c7b83832db5aada55bfdf2
jahjahLemonade <jspades93@gmail.com>
2025-07-19 18:56:22 +0300

    Debugging Server

--- src/controller/stripe.js
+++ src/controller/stripe.js
@@ -298,11 +298,11 @@ export const testStripeWebHook = async (req, res) => {
         console.log("Subscription details:", subscription);
         const customerId = subscription.customer;
         console.log("Customer ID:", customerId);
-        const emailId = subscription.customer_email
+        const emailId = (await stripe.customers.retrieve(customerId)).email;
         console.log("Customer Email:", emailId);
         const subscriptionId = subscription.id;
         console.log("Subscription ID:", subscriptionId);
-        const paymentIntentId = subscription.latest_invoice.payment_intent;
+        const paymentIntentId = (await stripe.subscriptions.retrieve(subscriptionId)).latest_invoice.payment_intent;
         console.log("Payment Intent ID:", paymentIntentId);
         if (!subscriptionId) {
           console.log("> ", "No subscription ID found, retrieving subscription details");
